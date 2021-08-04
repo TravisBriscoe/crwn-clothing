1462ad3b8e332d5aa507c0878a2412f43318d077
AnomicAphasia <travis.briscoe@gmail.com>
2021-08-03 23:07:31 -0400

    Heroku Push

---
+++ src/firebase/firebase.utils.jsx
@@ -0,0 +1,51 @@
+import firebase from 'firebase/app';
+
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyAneDc9ir-BM97a_W3v2TCkbtdM3TlCxzA",
+  authDomain: "crwn-clothing-701b1.firebaseapp.com",
+  projectId: "crwn-clothing-701b1",
+  storageBucket: "crwn-clothing-701b1.appspot.com",
+  messagingSenderId: "751615091125",
+  appId: "1:751615091125:web:131f1cbb8e21ecb36ff9fc"
+};
+
+firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { email, displayName } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (err) {
+      console.log('error creating user', err.message);
+    }
+
+  }
+  
+  return userRef;
+}
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
---
+++ src/redux/store.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
+import logger from 'redux-logger';
+
+import rootReducer from './root-reducer';
+
+const middleWares = [logger];
+
+export const store = createStore(rootReducer, applyMiddleware(...middleWares));
+
+export const persistor = persistStore(store);
+
+const exportStuff = { store, persistor };
+
+export default exportStuff;
